a816e23e5adcdc6326984048fed416f6e97d9a21
meryem.sekkat <meryem.sekkat@alithya.com>
2022-09-22 16:10:52 +0100

    add more features

--- src/main/java/com/ltp/gradesubmission/Grade.java
+++ src/main/java/com/ltp/gradesubmission/Grade.java
@@ -2,9 +2,15 @@ package com.ltp.gradesubmission;
 
 import java.util.UUID;
 
+import javax.validation.constraints.NotBlank;
+
 public class Grade {
+
+    @NotBlank(message = "Name cannot be blank")
     private String name;
+    @NotBlank(message = "Subject cannot be blank")
     private String subject;
+    @Score(message = "Score must be a letter grade")
     private String score;
     private String id;
 
